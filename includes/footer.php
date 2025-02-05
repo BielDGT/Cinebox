d30dc68970b96404e04f06f618c999566e80145f
BielDGT <david.tarleyy@gmail.com>
2025-02-05 11:37:57 -0300

    aprendendo php
    utilização do php para header (refatoração do codigo)

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+</body>
+<script src="./Assets/Js/carrossel.js"></script>
+<script src="./Assets/Js/offcanvas.js"></script>
+</html>
--- index.php
+++ index.php
@@ -1,47 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+    <?php include './includes/header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CineBox</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./Assets/CSS/Style.css">
-    <link rel="stylesheet" href="./Assets/CSS/Carrossel.css">
-    <link rel="stylesheet" href="./Assets/CSS/Filmes.css">
-</head>
 
-<body>
-    <header>
-        <nav>
-            <a href="#" class="logo">
-                <h1>CineBox</h1>
-            </a>
-            <ul class="menu">
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Filmes</a></li>
-                <li><a href="#">Sobre</a></li>
-            </ul>
-            <ul class="menu-icone">
-                <li><a href="#"><i class="bi bi-search"></i></a></li>
-                <li><a href="#"><i class="bi bi-heart"></i></a></li>
-                <li><a href="#"><i class="bi bi-cart"></i></a></li>
-                <li><a href="#"><i class="bi bi-person-circle"></i></a></li>
-            </ul>
-            
-            <button class="menu-oculto" onclick="javascript:abrirnav()"><i class="bi bi-list"></i></button>
-
-            <div id="offcanvas" class="menu-oculto">
-                <button class="fechar" onclick="javascript:fecharnav()"><i class="bi bi-x"></i></button>
-                <a href="index.html">Inicio</a>
-                <a href="#">Filmes</a>
-                <a href="#">Sobre</a>
-            </div>
-
-        </nav>
-    </header>
     <section id="banner">
         <main class="carrossel-container">
             <div class="carrossel">
@@ -208,7 +168,3 @@
         </main>
     </section>
 
-</body>
-<script src="./Assets/Js/carrossel.js"></script>
-<script src="./Assets/Js/offcanvas.js"></script>
-</html>
